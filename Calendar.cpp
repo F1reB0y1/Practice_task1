f3b9ce73f1b6b0c81abbf4f074e84b98988cca09
F1reB0y1 <157581148+F1reB0y1@users.noreply.github.com>
2024-09-20 13:57:53 +0300

    Add files via upload

---
+++ Calendar.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("   March 2024\n");
+    printf("Mo Tu We Th Fr Sa Su\n");
+    printf("             1  2 3 \n");
+    printf(" 4  5  6  7  8  9 10\n");
+    printf("11 12 13 14 15 16 17\n");
+    printf("18 19 20 21 22 23 24\n");
+    printf("25 26 27 28 29 30 31\n");
+    
+    return 0;
+}
